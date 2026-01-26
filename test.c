6cae422bc784ed9dfb6e6b132ba1aa223af72702
coder-mehedi-hasan <mh8399807@gmail.com>
2026-01-27 00:55:03 +0600

    refactor: improve IP and network configuration input handling

--- test.c
+++ test.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+char *takeValidIp();
+int takeValidNetworksCount();
+struct networkConfiguration takeConfigurationInput(int networkNo);
+struct IpCidr splitIpWithCidr(const char *ip);
+bool isValidIp(char ip[]);
+
+
 struct networkConfiguration
 {
     int networkNo;
@@ -18,23 +25,24 @@ struct IpCidr
 
 int main()
 {
-
     int networkCount;
-    // Take user input::
-    char ip[26] = takeValidIp();
 
-    return 0;
+    // Take user input
+    char *ip = takeValidIp();
+    printf("IP entered: %s\n", ip);
     // how many networks you need ?:
-    printf("how many networks you need ?");
-    scanf("%d", &networkCount);
+    networkCount = takeValidNetworksCount();
     // for each network, how many hosts you need ?
     struct networkConfiguration networks[networkCount];
     for (int i = 1; i <= networkCount; i++)
     {
-        networks[i - 1] = takeInput(i);
+        networks[i - 1] = takeConfigurationInput(i);
     }
 
-    // Check input is valid
+    for (int i = 1; i <= networkCount; i++)
+    {
+        printf("Network %d needs %d hosts\n", networks[i - 1].networkNo, networks[i - 1].hostNeed);
+    }
 
     // Check IP is network IP
 
@@ -47,34 +55,48 @@ int main()
     return 0;
 }
 
+int takeValidNetworksCount(){
+    int count;
+    while (true)
+    {
+        printf("how many networks you need ? ");
+        scanf("%d", &count);
+        if (count > 0)
+            break;
+        printf("please give a valid network count\n");
+    }
+    return count;
+}
+
 char *takeValidIp()
 {
-    char ip[26];
-    printf("what is the IP address: ");
-    scanf("%s", ip);
-    if (!isValidIp(ip))
+    static char ip[26];
+    while (true)
     {
+        printf("what is the IP address with cidr: ");
+        scanf("%25s", ip);
+        if (isValidIp(ip))
+            break;
         printf("please give valid ip addresss.\n");
-        return takeValidIp();
     }
     return ip;
 }
 
-struct networkConfiguration takeInput(int networkNo)
+struct networkConfiguration takeConfigurationInput(int networkNo)
 {
     struct networkConfiguration config;
     config.networkNo = networkNo;
-    // How many hosts you need ?
-    printf("How many hosts you need for network %d: ", networkNo);
-    scanf("%d", &config.hostNeed);
-    if (config.hostNeed <= 0)
+    // how many hosts you need ?
+    while (true)
     {
-        printf("please enter a valid hosts");
-        return takeInput(networkNo);
+        printf("how many hosts you need for network %d: ", networkNo);
+        scanf("%d", &config.hostNeed);
+        if (config.hostNeed > 0)
+            break;
+        printf("please give a valid host\n");
     }
     return config;
 }
-
 
 struct IpCidr splitIpWithCidr(const char *ip)
 {
@@ -86,7 +108,7 @@ struct IpCidr splitIpWithCidr(const char *ip)
     int count = 0;
     int cidr = -1;
 
-    // read IP with cider
+    // read IP with cird
     count = sscanf(ip, "%d.%d.%d.%d/%d",
                    &result.parts[0],
                    &result.parts[1],
@@ -124,6 +146,7 @@ struct IpCidr splitIpWithCidr(const char *ip)
 bool isValidIp(char ip[])
 {
     struct IpCidr ipCidr = splitIpWithCidr(ip);
+
     if (ipCidr.cidr == -1)
     {
         return false;
@@ -136,6 +159,5 @@ bool isValidIp(char ip[])
     {
         return false;
     }
-
     return true;
 }
