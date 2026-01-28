5052cb202c80179b83d5db92452d3160475c13ba
coder-mehedi-hasan <mh8399807@gmail.com>
2026-01-28 11:41:49 +0600

    feat: implement subnet mask calculation and network address validation

--- test.c
+++ test.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 
 char *takeValidIp();
 int takeValidNetworksCount();
 struct networkConfiguration takeConfigurationInput(int networkNo);
 struct IpCidr splitIpWithCidr(const char *ip);
 bool isValidIp(char ip[]);
-
+bool isValidNetworkAddress(char ip[], char subnetMask[]);
+char *calculateSubnetMask(char ip[], int cidr);
 
 struct networkConfiguration
 {
@@ -43,9 +46,17 @@ int main()
     {
         printf("Network %d needs %d hosts\n", networks[i - 1].networkNo, networks[i - 1].hostNeed);
     }
-
+    char *subnetMask = calculateSubnetMask(ip, -1);
+    printf("Subnet Mask: %s\n", subnetMask);
     // Check IP is network IP
-
+    if (isValidNetworkAddress(ip, subnetMask))
+    {
+        printf("The IP %s is a valid network address for subnet mask %s\n", ip, subnetMask);
+    }
+    else
+    {
+        printf("The IP %s is NOT a valid network address for subnet mask %s\n", ip, subnetMask);
+    }
     // For each network
 
     // calculate cidr
@@ -55,7 +66,82 @@ int main()
     return 0;
 }
 
-int takeValidNetworksCount(){
+bool isValidNetworkAddress(char ip[], char subnetMask[])
+{
+    struct IpCidr ipCidr = splitIpWithCidr(ip);           // get ip parts
+    struct IpCidr maskCidr = splitIpWithCidr(subnetMask); // get subnet mask parts
+
+    if (ipCidr.cidr == -1)
+    {
+        return false;
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        int networkPart = ipCidr.parts[i] & maskCidr.parts[i];
+
+        if (networkPart != ipCidr.parts[i])
+            return false;
+    }
+
+    return true;
+}
+
+char *calculateSubnetMask(char ip[], int cidr)
+{
+    if (cidr < 0 || cidr > 32)
+    {
+        // Get cidr from ip
+        struct IpCidr ipCidr = splitIpWithCidr(ip);
+        cidr = ipCidr.cidr;
+    }
+
+    static char subnetMask[15];
+
+    // create 32 bit binary mask
+    char binaryMask[32];
+    for (int i = 0; i < 32; i++)
+    {
+        if (i < cidr)
+        {
+            binaryMask[i] = '1';
+        }
+        else
+        {
+            binaryMask[i] = '0';
+        }
+    }
+
+    // uterate 4 parts and make decimal values
+    int parts[4] = {0, 0, 0, 0};
+    int partIndex = 0;
+    int startIndex = 0;
+    for (int i = 8; i <= 32; i += 8)
+    {
+        char _bitsPart[8];
+        int j = 0;
+        while (startIndex < i)
+        {
+            _bitsPart[j] = binaryMask[startIndex];
+            j++;
+            startIndex++;
+        }
+        char *endptr;
+        long decimal_integer;
+        // Convert the binary string to a long integer
+        decimal_integer = strtol(_bitsPart, &endptr, 2);
+        startIndex = i;
+        parts[partIndex] = (int)decimal_integer;
+        partIndex++;
+    }
+
+    // make subnet mask string
+    sprintf(subnetMask, "%d.%d.%d.%d", parts[0], parts[1], parts[2], parts[3]);
+
+    return subnetMask;
+}
+
+int takeValidNetworksCount()
+{
     int count;
     while (true)
     {
